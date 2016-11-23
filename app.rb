706642d61475b271cbb67e51947b88f4d72b7c13
Kim Wilson <k.wilson541@gmail.com>
2016-11-23 14:18:11 +0000

    Refactored to store params in a session

--- app.rb
+++ app.rb
@@ -1,14 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+
+    redirect '/play'
+  end
+
+  get '/play' do
     p params
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:play)
   end
 
