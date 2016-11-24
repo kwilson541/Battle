b336de6946064be3fc527bfec1d186864973eb99
keomony khun <keomony@gmail.com>
2016-11-24 13:41:50 +0000

    Attacking Player 2

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "Attack" do
+  it "should redirect to /attack" do
+    sign_in_and_play
+    click_button "Attack"
+    expect(page.current_path).to eq "/attack"
+  end
+
+end
