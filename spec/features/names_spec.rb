75abc1ef81828d234ff49c9c3a45bfc2cb5eefac
keomony khun <keomony@gmail.com>
2016-11-24 11:19:41 +0000

    refactors names_spec.rb

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 describe "Entering names", :type => :feature do
-  # include Capybara::DSL
-  it "fills in names of players" do
+
+  before do
     visit "/"
 
     fill_in "player1_name", :with => "Bill"
     fill_in "player2_name", :with => "Foo"
     click_button "SUBMIT"
+  end
 
+  it "redirects to /play path" do
     expect(page.current_path).to eq("/play")
+  end
+
+  it "fills in name of player 1" do
     expect(page).to have_text("Bill")
+  end
+
+  it "fills in name of player 2" do
     expect(page).to have_text("Foo")
   end
 end
